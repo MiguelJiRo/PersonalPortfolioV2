3a9c9e71810b2ef3934e18c73ffec30f09134d14
Miguel Jiménez Rodríguez <migueljiroz@gmail.com>
2024-01-05 00:23:04 +0100

    Updates

--- aa0eb7ca4da7758f807f.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app";
-var container = document.getElementById("root");
-var root = createRoot(container);
-root.render( /*#__PURE__*/React.createElement(App, null));
